4043fdd0b7f26a7223c3e0fb7e776170e6123e2b
syuilo <syuilotan@yahoo.co.jp>
2016-09-07 23:55:29 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const httpProxy = require('http-proxy');
+
+const proxy = httpProxy.createProxyServer({});
+
+const server = http.createServer((req, res) => {
+	const domain = req.headers.host.replace('misskey.xyz', '');
+	switch (domain) {
+		case '':
+		case 'about':
+		case 'signin':
+		case 'signout':
+		case 'signup':
+		case 'status':
+		case 'talk':
+			proxy.web(req, res, { target: 'http://localhost:8000' });
+			break;
+		case 'api':
+			proxy.web(req, res, { target: 'http://localhost:8001' });
+			break;
+		case 'file':
+			proxy.web(req, res, { target: 'http://localhost:8002' });
+			break;
+		default:
+			console.log(`Unknown domain: ${domain}`);
+			break;
+	}
+});
+
+server.listen(80);
