b57964b8963a50d31d65c6fd4c468990ddef63c2
syuilo <syuilotan@yahoo.co.jp>
2016-09-11 21:00:06 +0900

    Move error handling logic to config.json

--- src/config.js
+++ src/config.js
@@ -13,4 +13,16 @@ const configPath = `${configDirPath}${path.sep}config.json`;
 /**
  * LOAD CONFIG
  */
-module.exports = () => JSON.parse(fs.readFileSync(configPath, 'utf8'));
+let config;
+try {
+	config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
+} catch (e) {
+	if (e.code !== 'ENOENT') {
+		console.error('Failed to load configuration');
+		process.exit(1);
+	}
+	console.error('Config not found');
+	process.exit(1);
+}
+
+module.exports = config;
