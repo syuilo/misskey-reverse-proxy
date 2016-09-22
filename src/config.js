b380a1fa72140884d198614bd0c184196b7a05f6
syuilo <syuilotan@yahoo.co.jp>
2016-09-22 21:00:15 +0900

    :v:

--- src/config.js
+++ src/config.js
@@ -1,28 +1,9 @@
-/**
- * MODULE LOADS
- */ 
 const path = require('path');
 const fs = require('fs');
 
-/**
- * META CONSTS
- */
 const configDirPath = `${__dirname}${path.sep}..${path.sep}.config`
 const configPath = `${configDirPath}${path.sep}config.json`;
 
-/**
- * LOAD CONFIG
- */
-let config;
-try {
-	config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
-} catch (e) {
-	if (e.code !== 'ENOENT') {
-		console.error('Failed to load configuration');
-		process.exit(1);
-	}
-	console.error('Config not found');
-	process.exit(1);
-}
+const config = require(configPath);
 
 module.exports = config;
