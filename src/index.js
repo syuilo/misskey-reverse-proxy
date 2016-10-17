e488e6cf30fe8fb60ff51134217d05027b3b2414
syuilo <syuilotan@yahoo.co.jp>
2016-10-18 03:57:09 +0900

    Cluster

--- src/index.js
+++ src/index.js
@@ -1,56 +1,17 @@
-const http = require('http');
-const httpProxy = require('http-proxy');
-const config = require('./config');
-const host = 'localhost';
+const cluster = require('cluster');
 
-const proxy = httpProxy.createProxyServer({});
+// Master
+if (cluster.isMaster) {
 
-const server = http.createServer((req, res) => {
-	const forwarded = req.headers['X-Forwarded-Proto'];
+	// Count the machine's CPUs
+	const cpuCount = require('os').cpus().length;
 
-	if (forwarded && forwarded === 'http' && config.https) {
-		res.writeHead(301, {
-			'Location': 'https://' + req.host + req.originalUrl
-		});
-		res.end();
-		return;
+	// Create a worker for each CPU
+	for (let i = 0; i < cpuCount; i++) {
+		cluster.fork();
 	}
-
-	const reqHost = req.headers.host;
-
-	if (reqHost == null) {
-		return;
-	}
-
-	switch (reqHost) {
-		case 'misskey.xyz':
-		case 'm.misskey.xyz':
-		case 'mobile.misskey.xyz':
-		case 'about.misskey.xyz':
-		case 'signin.misskey.xyz':
-		case 'signout.misskey.xyz':
-		case 'signup.misskey.xyz':
-		case 'status.misskey.xyz':
-		case 'talk.misskey.xyz':
-			proxy.web(req, res, { target: `http://${host}:${config.ports.web}` });
-			break;
-		case 'api.misskey.xyz':
-			proxy.web(req, res, { target: `http://${host}:${config.ports.core}` });
-			break;
-		case 'himasaku.net':
-			proxy.web(req, res, { target: `http://${host}:${config.ports.file}` });
-			break;
-		case 'proxy.himasaku.net':
-			proxy.web(req, res, { target: `http://${host}:${config.ports.proxy}` });
-			break;
-		default:
-			console.log(`Unknown host: ${reqHost}`);
-			break;
-	}
-});
-
-server.on('upgrade', (req, socket, head) => {
-  proxy.ws(req, socket, head, { target: `ws://${host}:${config.ports.core}` });
-});
-
-server.listen(config.port);
+}
+// Workers
+else {
+	require('./server');
+}
---
+++ src/server.js
@@ -0,0 +1,56 @@
+const http = require('http');
+const httpProxy = require('http-proxy');
+const config = require('./config');
+const host = 'localhost';
+
+const proxy = httpProxy.createProxyServer({});
+
+const server = http.createServer((req, res) => {
+	const forwarded = req.headers['X-Forwarded-Proto'];
+
+	if (forwarded && forwarded === 'http' && config.https) {
+		res.writeHead(301, {
+			'Location': 'https://' + req.host + req.originalUrl
+		});
+		res.end();
+		return;
+	}
+
+	const reqHost = req.headers.host;
+
+	if (reqHost == null) {
+		return;
+	}
+
+	switch (reqHost) {
+		case 'misskey.xyz':
+		case 'm.misskey.xyz':
+		case 'mobile.misskey.xyz':
+		case 'about.misskey.xyz':
+		case 'signin.misskey.xyz':
+		case 'signout.misskey.xyz':
+		case 'signup.misskey.xyz':
+		case 'status.misskey.xyz':
+		case 'talk.misskey.xyz':
+			proxy.web(req, res, { target: `http://${host}:${config.ports.web}` });
+			break;
+		case 'api.misskey.xyz':
+			proxy.web(req, res, { target: `http://${host}:${config.ports.core}` });
+			break;
+		case 'himasaku.net':
+			proxy.web(req, res, { target: `http://${host}:${config.ports.file}` });
+			break;
+		case 'proxy.himasaku.net':
+			proxy.web(req, res, { target: `http://${host}:${config.ports.proxy}` });
+			break;
+		default:
+			console.log(`Unknown host: ${reqHost}`);
+			break;
+	}
+});
+
+server.on('upgrade', (req, socket, head) => {
+  proxy.ws(req, socket, head, { target: `ws://${host}:${config.ports.core}` });
+});
+
+server.listen(config.port);
